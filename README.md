9e53c4800f0fcce9fca863f3d2bba0d8ba42d1a9
Björn Roberg <bjorn.roberg@gmail.com>
2015-03-06 14:52:47 +0100

    Added webpack-dev-server
    Change-Id: I3b8e08d667fc30c7e4e6b02e2939283ce1fcc88a

---
+++ README.md
@@ -0,0 +1,21 @@
+# Quick JS
+
+A skeleton project using webpack and some nice loaders.
+
+# Installing deps
+
+`npm install` should work
+
+if on windows, you get an error relating to `node-gyp`, please try
+
+`npm install --msvs_version=%VISUAL_STUDIO_VERSSION`
+
+where `%VISUAL_STUDIO_VERSSION` is perhaps `2013` or `2012`
+
+# Building
+
+`npm run compile`
+
+# Running a dev server
+
+`npm run serve` in the root directory.
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
 module.exports = {
-  devServer: true,
   devtool: 'eval',
   debug: true,
-  entry: "./src/index.js",
-  output: {
-    path: __dirname + '/src/',
-    filename: "bundle.js"
+
+  entry: {
+    app: ['webpack/hot/dev-server', './src/index.js']
   },
+  output: {
+    path: './build',
+    filename: 'bundle.js'
+  },
+
   module: {
     loaders: [
       { test: /\.js$/, loader: 'esnext' },
-      { test: /\.css$/, loader: "style!css" }
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.html$/, loader: "html" }
     ]
   }
 };
