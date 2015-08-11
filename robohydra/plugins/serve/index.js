d85d21218221139e3efdb89a5130284395241b9f
Björn Roberg <bjorn.roberg@gmail.com>
2015-08-11 20:17:50 +0200

    Fix robohydra proxy to potential api

--- robohydra/plugins/serve/index.js
+++ robohydra/plugins/serve/index.js
@@ -1,17 +1,21 @@
 var heads = require('robohydra').heads;
-var RoboHydraHeadFilesystem = heads.RoboHydraHeadFilesystem;
 var RoboHydraHeadProxy = heads.RoboHydraHeadProxy;
+var RoboHydraHeadFilesystem = heads.RoboHydraHeadFilesystem;
 
 exports.getBodyParts = function(conf) {
   return {
     heads: [
+      // The order of heads are important.
+      // Most specific to the top.
+      // Most general to the bottom.
+      new RoboHydraHeadProxy({
+        mountPath: '/api',
+        proxyTo: 'http://localhost:3030/api/v1',
+        setHostHeader: true
+      }),
       new RoboHydraHeadFilesystem({
         mountPath: '/',
         documentRoot: 'dist'
-      }),
-      new RoboHydraHeadProxy({
-        mountPath: '/api',
-        proxyTo: 'http://localhost:9090/api/v1'
       })
     ]
   };
